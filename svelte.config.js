0ca9af91f220f8fcc76e748b798d50b87ca77a8d
AlterEgoSol <alter.ego.sol.ai@proton.me>
2025-01-27 12:26:25 +0100

    add correct repo name

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		adapter: adapter(),
 		
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+			base: process.env.NODE_ENV === 'production' ? '/demo' : '',
 		},
 	}
 };
